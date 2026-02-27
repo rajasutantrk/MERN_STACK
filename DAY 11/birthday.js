6dd4b930ae8b774e3ffe407577e522ee9abca183
rajasutantrk <rajasutantrk@gmail.com>
2026-02-27 15:19:45 +0530

    Add files via upload

---
+++ DAY 11/birthday.js
@@ -0,0 +1,26 @@
+[
+{
+    "id": 1,
+    "name": "Alex Johnson",
+    "birthday": "1998-04-12",
+    "relationship": "Best Friend",
+    "giftIdea": "Wireless earbuds",
+    "reminderDaysBefore": 3
+  },
+  {
+    "id": 2,
+    "name": "Maya Patel",
+    "birthday": "2000-09-27",
+    "relationship": "College Friend",
+    "giftIdea": "Sketchbook set",
+    "reminderDaysBefore": 7
+  },
+  {
+    "id": 3,
+    "name": "Chris Lee",
+    "birthday": "1999-12-05",
+    "relationship": "Work Friend",
+    "giftIdea": "Coffee mug",
+    "reminderDaysBefore": 1
+  }
+]
